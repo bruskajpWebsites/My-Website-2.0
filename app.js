a530ece5403899cccadd63a0d651695b40878b9b
J B <jackedup102@hotmail.com>
2016-05-04 20:30:40 -0400

    Have javascript for header working for tabs and it displays the subsections

--- app.js
+++ app.js
@@ -1,5 +1,67 @@
-var main = function () {
+"use strict";
 
+var main = function () {
+  var click = false;
+  var justClicked = false;
+  var hover = false;
+  var subhover = false;
+
+  $('.dropdown-toggle').click(function () {
+    if (($(this).next()).is(":visible") && !hover) {
+      $(this).next().hide();
+      click = false
+      justClicked = true;
+    } else if (($(this).next()).is(":visible") && hover) {
+      click = true;
+      hover = false;
+    } else {
+      $(this).next().show();
+      click = true;
+    }
+  });
+
+  $('.dropdown-toggle').hover(function () {
+    if (!click) {
+      if (($(this).next()).is(":visible")) {
+        var temp = this;
+        setTimeout(function (thing) {
+          if (!subhover) {
+            $(temp).next().hide();
+          }
+        }, 100);
+        hover = false;
+      } else {
+        if (!justClicked) {
+          $(this).next().show();
+          hover = true;
+        } else {
+          justClicked = false;
+        }
+      }
+    }
+  });
+
+  $('.dropdown-menu').hover(function () {
+    if (!click) {
+      if (($(this)).is(":visible")) {
+        var temp = this;
+        setTimeout(function () {
+          if (!subhover) {
+            $(temp).hide();
+          }
+          subhover = false;
+        }, 100);
+      } else {
+        $(this).show();
+        subhover = true;
+      }
+    }
+  });
+
+  $('.dropdown-menu').click(function () {
+    // check if this referes to the menu or the link (assumes link)
+    $(this)
+  });
 }
 
 $(document).ready(main);
