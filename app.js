b2ac2ad906bf699b69722ec21848abad5c933c0f
J B <jackedup102@hotmail.com>
2016-05-12 17:43:12 -0400

    Added all of the content and added the contact me forms

--- app.js
+++ app.js
@@ -60,8 +60,56 @@ var main = function () {
 
   $('.dropdown-menu').click(function () {
     // check if this referes to the menu or the link (assumes link)
-    
+
   });
+
+  $('.seeMore').click(function () {
+    var seeMoreText = $(this).parent().children('.seeMoreText');
+    if (seeMoreText.is(":visible")) {
+      //$(this).parent().children('.seeMoreText').hide();
+      seeMoreText.hide();
+      $(this).text("See More");
+    } else {
+      //$(this).parent().children('.seeMoreText').show();
+      seeMoreText.show();
+      $(this).text("See Less");
+    }
+  });
+
+  $('.submit').click(function () {
+    var name = $('.name').val();
+    var email = $('.email').val();
+    var message = $('.message').val();
+    $('.name').val('');
+    $('.email').val('');
+    $('.message').val('');
+    $('.counter').text('280');
+    $('.submit').addClass('disabled');
+  });
+
+  var checkToSend = function () {
+    var nameLength = $('.name').val().length;
+    var emailLength = $('.email').val().length;
+    var messageLength = $('.message').val().length;
+    var charactersLeft = 280 - messageLength;
+    $('.counter').text(charactersLeft);
+    if (nameLength > 0 && emailLength > 0) {
+      if (charactersLeft < 0) {
+        $('.submit').addClass('disabled');
+      } else if (charactersLeft === 280) {
+        $('.submit').addClass('disabled');
+      } else {
+        $('.submit').removeClass('disabled');
+      }
+    }
+  }
+
+  $('.name').keyup(checkToSend);
+  $('.email').keyup(checkToSend);
+  $('.message').keyup(checkToSend);
+
+  $('.submit').addClass('disabled');
 }
+
 
 $(document).ready(main);
